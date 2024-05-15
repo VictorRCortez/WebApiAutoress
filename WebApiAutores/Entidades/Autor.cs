e273e86935ac364d7700d0cb2e19adf27a5a6546
VictorRCortez <renecortez871@gmail.com>
2024-05-15 14:00:01 -0600

    Agregar archivos de proyecto.

---
+++ WebApiAutores/Entidades/Autor.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApiAutores.Entidades
+{
+    public class Autor
+    {
+        //crear las propiedades
+        //prop es un sniper para crear de una manera rapidamente una propiedad
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public List<Libro> Libros { get; set; } 
+
+
+    }
+}
---
+++ WebApiAutores/Program.cs
@@ -0,0 +1,19 @@
+using WebApiAutores;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var startup = new Startup(builder.Configuration);
+
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+startup.configure(app, app.Environment);
+app.Run();
